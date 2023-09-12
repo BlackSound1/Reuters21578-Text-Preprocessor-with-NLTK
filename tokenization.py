c8d7b4cf2aebb21033a0c19c678df6a17cca7350
BlackSound1 <matthewr@videotron.ca>
2023-09-11 21:18:36 -0400

    Removed unused code

--- tokenization.py
+++ tokenization.py
@@ -1,5 +1,4 @@
 from typing import List
-from pathlib import Path
 
 from nltk import word_tokenize
 
@@ -13,15 +12,5 @@ def tokenize(text: str, article_num: int) -> List[str]:
         print(f"Article {article_num}: writing to file Tokenizer-output")
 
         write_to_file(article_num, "Tokenizer-output", tokenized)
-
-        # DIR_NAME = f"output/article{article_num}"
-        #
-        # FILENAME = f"{DIR_NAME}/Tokenizer-output.txt"
-        #
-        # Path(DIR_NAME).mkdir(parents=True, exist_ok=True)
-        #
-        # with open(FILENAME, "w+t", encoding="utf-8") as file:
-        #     # Write each entry in the list line-by-line
-        #     file.writelines([i + '\n' for i in tokenized])
 
     return tokenized
